92ffa05cd67205f2b356324907d266c8bfa55456
Rafael Meloni <rafaelmeloni@gmail.com>
2025-06-28 16:14:26 -0300

    FindAll controller

--- src/main/java/com/rafaelmeloni/crud_demo/controllers/ClientController.java
+++ src/main/java/com/rafaelmeloni/crud_demo/controllers/ClientController.java
@@ -1,10 +1,10 @@
 package com.rafaelmeloni.crud_demo.controllers;
 
 import com.rafaelmeloni.crud_demo.dto.ClientDTO;
-import com.rafaelmeloni.crud_demo.entities.Client;
-import com.rafaelmeloni.crud_demo.repositories.ClientRepository;
 import com.rafaelmeloni.crud_demo.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,11 +19,21 @@ public class ClientController {
     private ClientService clientService;
 
 
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dtos = clientService.findAll(pageable);
+        return ResponseEntity.ok(dtos);
+
+    }
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Integer id){
         ClientDTO dto = clientService.findById(id);
         return ResponseEntity.ok(dto);
     }
+
+
+
 
 
 }
--- src/main/java/com/rafaelmeloni/crud_demo/services/ClientService.java
+++ src/main/java/com/rafaelmeloni/crud_demo/services/ClientService.java
@@ -6,6 +6,8 @@ import com.rafaelmeloni.crud_demo.entities.Client;
 import com.rafaelmeloni.crud_demo.repositories.ClientRepository;
 import com.rafaelmeloni.crud_demo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +22,14 @@ public class ClientService {
 
        Client client = clientRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Resource not found with id " + id));
        return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+
+        Page<Client> clientList = clientRepository.findAll(pageable);
+        return clientList.map(ClientDTO::new);
+
     }
 
 
